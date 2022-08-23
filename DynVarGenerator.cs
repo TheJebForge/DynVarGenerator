ad2769b69725bda53562be8ba70ca419cf9773fd
TheJebForge <jeb@thejebforge.com>
2022-08-23 20:27:43 +0300

    Updated NeosMod version

--- DynVarGenerator.cs
+++ DynVarGenerator.cs
@@ -7,7 +7,7 @@ namespace DynVarGenerator
     {
         public override string Name => "DynVarGenerator";
         public override string Author => "TheJebForge";
-        public override string Version => "1.0.0";
+        public override string Version => "1.1.0";
 
         public override void OnEngineInit()
         {
